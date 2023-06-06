688690bd08cb8c8ff7fabbe0c7cb13959d76fead
irene.yu <irene.xin.yu@gmail.com>
2023-06-05 18:39:34 -0700

    Create project

---
+++ example/chinese_lunar_calendar_example.dart
@@ -0,0 +1,6 @@
+import 'package:chinese_lunar_calendar/chinese_lunar_calendar.dart';
+
+void main() {
+  var awesome = Awesome();
+  print('awesome: ${awesome.isAwesome}');
+}
---
+++ lib/chinese_lunar_calendar.dart
@@ -0,0 +1,3 @@
+library chinese_lunar_calendar;
+
+export 'src/chinese_lunar_calendar_base.dart';
