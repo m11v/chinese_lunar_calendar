15021d0d5af559783497207bd1ea96efcc881f34
Irene Yu <irene.xin.yu@gmail.com>
2023-06-07 15:15:47 -0700

    Update readme (#8)

--- example/chinese_lunar_calendar_example.dart
+++ example/chinese_lunar_calendar_example.dart
@@ -2,7 +2,9 @@ import 'package:chinese_lunar_calendar/chinese_lunar_calendar.dart';
 
 void main() {
   final lunarCalendar = LunarCalendar(dateTime: DateTime(2023, 6, 6));
+  print('2023年6月6日');
   print('春节: ${lunarCalendar.chineseNewYearString}');
-  print('阴历数字: ${lunarCalendar.lunarDate}');
-  print('农历: ${lunarCalendar.lunarDate.fullCNString}');
+  print(
+      '阴历数字: ${lunarCalendar.lunarDate.year}, ${lunarCalendar.lunarDate.month}, ${lunarCalendar.lunarDate.day}, 闰月：${lunarCalendar.lunarDate.isLeapMonth}');
+  print('阴历汉字: ${lunarCalendar.lunarDate.fullCNString}');
 }
