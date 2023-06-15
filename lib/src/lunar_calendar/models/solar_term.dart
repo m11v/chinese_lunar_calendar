169d09aa65d24f8d91b1d4be8bce9410f5a4e2bb
Irene Yu <irene.xin.yu@gmail.com>
2023-06-14 19:58:57 -0700

    Refactor SolarTerm (#27)

--- lib/src/lunar_calendar/models/solar_term.dart
+++ lib/src/lunar_calendar/models/solar_term.dart
@@ -1,42 +1,37 @@
 import 'package:chinese_lunar_calendar/chinese_lunar_calendar.dart';
 import 'package:equatable/equatable.dart';
 
+/// 节气
 class SolarTerm extends Equatable {
-  // Solar term name
+  /// Solar term name
   final String name;
 
-  // Time in milliseconds since epoch
-  final int fromMillisecondsSinceEpoch;
+  /// UTC time
+  final DateTime utc;
 
-  SolarTerm._internal(
-      {required this.name, required this.fromMillisecondsSinceEpoch});
+  SolarTerm._internal({required this.name, required this.utc});
 
-  // Create solar term from China Standard Time(UTC+8)
+  /// Create solar term from China Standard Time(UTC+8)
   factory SolarTerm.createFromCST(String name, int year, int month, int day,
       [int hour = 0, int minute = 0, int second = 0]) {
     final utc = parseCSTToUTC(year, month, day, hour, minute, second);
-    return SolarTerm._internal(
-        name: name, fromMillisecondsSinceEpoch: utc.millisecondsSinceEpoch);
+    return SolarTerm._internal(name: name, utc: utc);
   }
 
-  // UTC
-  DateTime get utc =>
-      DateTime.fromMillisecondsSinceEpoch(fromMillisecondsSinceEpoch).toUtc();
-
-  // Local time
+  /// Local time
   DateTime get local => utc.toLocal();
 
-  // China Standard Time
+  /// China Standard Time
   DateTime get cst => utc.add(Duration(hours: 8));
 
-  // Get China Standard Time or local time
+  /// Get China Standard Time or local time
   DateTime getTime({required bool useCSTToCalculate}) =>
       useCSTToCalculate ? cst : local;
 
   @override
-  List<Object?> get props => [name, fromMillisecondsSinceEpoch];
+  List<Object?> get props => [name, utc];
 
   @override
   String toString() =>
-      'SolarTerm(name: $name, ms: $fromMillisecondsSinceEpoch, CST: $cst, UTC: $utc, Local: $local)';
+      'SolarTerm(name: $name, CST: $cst, UTC: $utc, Local: $local)';
 }
