1fa6c6a27b909eb2553d3afea13b9e9fe972188b
Irene Yu <irene.xin.yu@gmail.com>
2024-06-27 15:45:41 -0700

    Fix Ke (#98)

--- test/lunar_calendar/models/ke_test.dart
+++ test/lunar_calendar/models/ke_test.dart
@@ -3,7 +3,7 @@ import 'package:test/test.dart';
 
 void main() {
   group('测试刻', () {
-    test('description', () {
+    test('测试刻', () {
       final ke1 = Ke.fromHour(hour: 23, minute: 05);
       expect(ke1.isUpper, true);
       expect(ke1.number, 1);
@@ -19,6 +19,14 @@ void main() {
       final ke4 = Ke.fromHour(hour: 00, minute: 55);
       expect(ke4.isUpper, false);
       expect(ke4.number, 4);
+
+      final ke5 = Ke.fromHour(hour: 23, minute: 00);
+      expect(ke5.isUpper, true);
+      expect(ke5.number, 1);
+
+      final ke6 = Ke.fromHour(hour: 23, minute: 30);
+      expect(ke6.isUpper, true);
+      expect(ke6.number, 3);
     });
   });
 }
