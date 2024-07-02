fcfd6ae2bce7df4c4d9574c5f1ed517f597d6e64
irene.yu <irene.xin.yu@gmail.com>
2024-07-01 19:08:49 -0700

    Refactor adjustedLunarYearByLichun to be the field of LunarCalendar

--- lib/src/lunar_calendar/lunar_calendar.dart
+++ lib/src/lunar_calendar/lunar_calendar.dart
@@ -18,6 +18,9 @@ class LunarCalendar extends Equatable {
   /// 本地大年初一（春节）日期
   late final DateTime chineseNewYear;
 
+  /// 以立春为一年之始时，调整后的阴历年份
+  late final int adjustedLunarYearByLichun;
+
   LunarCalendar._internal({
     required this.utc,
     required this.startZodiacFromLiChun,
@@ -36,7 +39,11 @@ class LunarCalendar extends Equatable {
       startZodiacFromLiChun: startZodiacFromLiChun,
     )
       ..lunarDate = lunarDate
-      ..chineseNewYear = chineseNewYear;
+      ..chineseNewYear = chineseNewYear
+      ..adjustedLunarYearByLichun = _getAdjustedLunarYearNumberByLiChun(
+          localTime: localTime,
+          chineseNewYear: chineseNewYear,
+          lunarYearNumber: lunarDate.lunarYear.number);
   }
 
   @override
@@ -48,6 +55,37 @@ class LunarCalendar extends Equatable {
       ];
 }
 
+/// 本程序遵循通用标准，以正月初一零时零分作为农历年的交替时刻
+/// 参见：https://www.hko.gov.hk/sc/%E5%A4%A9%E6%B0%94%E9%9A%8F%E7%AC%94/101852/%E5%86%9C%E5%8E%86%E5%B9%B4%E7%9A%84%E5%B9%B2%E6%94%AF%E4%BA%A4%E6%9B%BF
+/// 然而在计算月干支及生肖时，需要以立春为一年之始时，调整后的阴历年份作为参数。
+int _getAdjustedLunarYearNumberByLiChun({
+  required DateTime localTime,
+  required DateTime chineseNewYear,
+  required int lunarYearNumber,
+}) {
+  final local = localTime.millisecondsSinceEpoch;
+  final localLichun = localTime.lichun.local.millisecondsSinceEpoch;
+  final localChuyi = chineseNewYear.millisecondsSinceEpoch;
+
+  int adjustedYear = lunarYearNumber;
+
+  /// 立春在初一之前
+  if (localLichun < localChuyi) {
+    /// 日期在立春之后，在初一之前，需要+1
+    if (local < localChuyi && local > localLichun) {
+      adjustedYear += 1;
+    }
+  } else {
+    /// 立春在初一之后
+    /// 日期在初一之后，立春之前，需要 -1
+    if (local > localChuyi && local < localLichun) {
+      adjustedYear -= 1;
+    }
+  }
+
+  return adjustedYear;
+}
+
 extension LunarCalendarTime on LunarCalendar {
   /// 本地时间
   DateTime get localTime {
@@ -57,33 +95,6 @@ extension LunarCalendarTime on LunarCalendar {
   /// 中国标准时间
   DateTime get cst {
     return utcToCST(utc: utc);
-  }
-}
-
-extension LunarCalendarAdjusted on LunarCalendar {
-  /// 以立春为一年之始时，调整后的阴历年份
-  int get adjustedLunarYearByLichun {
-    final local = localTime.millisecondsSinceEpoch;
-    final localLichun = localTime.lichun.local.millisecondsSinceEpoch;
-    final localChuyi = chineseNewYear.toLocal().millisecondsSinceEpoch;
-
-    int adjustedYear = lunarDate.lunarYear.number;
-
-    /// 立春在初一之前
-    if (localLichun < localChuyi) {
-      /// 日期在立春之后，在初一之前，需要+1
-      if (local < localChuyi && local > localLichun) {
-        adjustedYear += 1;
-      }
-    } else {
-      /// 立春在初一之后
-      /// 日期在初一之后，立春之前，需要 -1
-      if (local > localChuyi && local < localLichun) {
-        adjustedYear -= 1;
-      }
-    }
-
-    return adjustedYear;
   }
 }
 
